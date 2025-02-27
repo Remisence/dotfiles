47251fa8995210d08c1cbc6521d4cc204cefdf7d
Remisence <RemingtonShowerd@proton.me>
2025-02-27 14:17:25 +0200

    deleted:    bspwm/bspwmrc 	deleted:    dunst/dunstrc 	deleted:    picom/picom.conf 	deleted:    polybar/bars.ini 	deleted:    polybar/colors.ini 	deleted:    polybar/config.ini 	deleted:    polybar/launch.sh 	deleted:    polybar/modules.ini 	deleted:    polybar/scripts/mic-volume.sh 	deleted:    polybar/scripts/pipewire-microphone.sh 	deleted:    polybar/scripts/polywins.sh 	deleted:    polybar/scripts/system-ati-radeontop.sh 	deleted:    polybar/scripts/updates-apt.sh 	deleted:    polybar/user_modules.ini 	deleted:    rofi/config.rasi 	deleted:    rofi/launcher/launcher.sh 	deleted:    rofi/launcher/theme/colors.rasi 	deleted:    rofi/launcher/theme/fonts.rasi 	deleted:    rofi/launcher/theme/simple.rasi 	deleted:    rofi/powermenu/powermenu.sh 	deleted:    rofi/powermenu/theme/colors.rasi 	deleted:    rofi/powermenu/theme/fonts.rasi 	deleted:    rofi/powermenu/theme/simple.rasi 	deleted:    rofi/simple-launcher/colors.rasi 	deleted:    rofi/simple-launcher/fonts.rasi 	deleted:    rofi/simple-launcher/launcher.sh 	deleted:    rofi/simple-launcher/simple-launcher.rasi 	deleted:    sxhkd/sxhkdrc

--- rofi/simple-launcher/launcher.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-dir="$HOME/.config/rofi/simple-launcher"
-theme="simple-launcher.rasi"
-
-## Run
-rofi -no-config -no-lazy-grab -show drun -modi drun \
-    -show drun \
-    -theme ${dir}/${theme}
